e17af7fe190369583ed96e2f3c0d1290d656324a
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-12-23 13:41:48 -0800

    added cohort_study_population template SQL

---
+++ irae/template/study_population.sql
@@ -0,0 +1,38 @@
+create table irae__cohort_study_population as
+with study_period as
+(
+    select distinct encounter_ref, subject_ref
+    from
+        core__encounter as E,
+        irae__include_study_period  as SP
+    where
+        (SP.include_history and E.period_start_day < SP.period_end) OR
+        (E.period_start_day between SP.period_start and SP.period_end)
+)
+select
+    E.age_at_visit,
+    E.gender,
+    E.race_display,
+    E.ethnicity_display,
+    E.class_code          as enc_class_code,
+    E.period_start_day    as enc_period_start_day,
+    E.period_end_day      as enc_period_end_day,
+    E.servicetype_code    as enc_servicetype_code,
+    E.servicetype_system  as enc_servicetype_system,
+    E.servicetype_display as enc_servicetype_display,
+    E.type_code           as enc_type_code,
+    E.type_system         as enc_type_system,
+    E.type_display        as enc_type_display,
+    E.encounter_ref,
+    E.subject_ref
+from
+    core__encounter                 as E,
+    study_period                    as SP,
+    irae__include_encounter_class   as enc_class,
+    irae__include_age_at_visit      as age
+where
+    (E.encounter_ref = SP.encounter_ref)  and
+    (E.class_code = enc_class.code)       and
+    (E.age_at_visit between age.age_min and age.age_max)
+;
+
