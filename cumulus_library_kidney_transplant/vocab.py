8a5322d085e4450cc0fa7edb9449909c955ccae9
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-08-11 12:53:27 -0700

    updated Vocab to include correct FHIR url for ICD10PCS (defined by CMS) and ICD9CM for historical data.

--- cumulus_library_kidney_transplant/vocab.py
+++ cumulus_library_kidney_transplant/vocab.py
@@ -1,8 +1,9 @@
 class Vocab:
     RXNORM = 'http://www.nlm.nih.gov/research/umls/rxnorm'
     LOINC = 'http://loinc.org'
+    ICD9CM = 'http://terminology.hl7.org/CodeSystem/icd9cm'
     ICD10CM = 'http://hl7.org/fhir/sid/icd-10'
-    ICD10PCS = 'http://www.icd10data.com/icd10pcs'
+    ICD10PCS = 'http://www.cms.gov/Medicare/Coding/ICD10'
     SNOMED = 'http://snomed.info/sct'
     CPT = 'http://www.ama-assn.org/go/cpt'
     HCPCS = 'http://www.nlm.nih.gov/research/umls/hcpcs'
