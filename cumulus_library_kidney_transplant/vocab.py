44fdf42f9dadd9d4c6f58f7f6f63137d6daa16fe
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-08-11 19:07:39 -0700

    VSAC valuesets use http://hl7.org/fhir/sid/icd-9-cm so lets assume that is the real URL defining (conflicting results from FHIR pages)
    https://build.fhir.org/ig/HL7/UTG/CodeSystem-ICD-9CM-diagnosiscodes.html

--- cumulus_library_kidney_transplant/vocab.py
+++ cumulus_library_kidney_transplant/vocab.py
@@ -1,7 +1,7 @@
 class Vocab:
     RXNORM = 'http://www.nlm.nih.gov/research/umls/rxnorm'
     LOINC = 'http://loinc.org'
-    ICD9CM = 'http://terminology.hl7.org/CodeSystem/icd9cm'
+    ICD9CM = 'http://hl7.org/fhir/sid/icd-9-cm'
     ICD10CM = 'http://hl7.org/fhir/sid/icd-10'
     ICD10PCS = 'http://www.cms.gov/Medicare/Coding/ICD10'
     SNOMED = 'http://snomed.info/sct'
