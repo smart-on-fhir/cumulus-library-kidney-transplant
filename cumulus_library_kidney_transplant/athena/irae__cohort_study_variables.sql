099045bdb36f7f6f40ebc5674f2101be4a09721a
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-08-25 18:00:18 -0700

    irae__cohort_study_variables.sql UNION ALL for faster performance (safe)

--- cumulus_library_kidney_transplant/athena/irae__cohort_study_variables.sql
+++ cumulus_library_kidney_transplant/athena/irae__cohort_study_variables.sql
@@ -1,34 +1,33 @@
 create table irae__cohort_study_variables as
 with variable_cohorts as
 (
-	select distinct 'doc_biopsy'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_doc_biopsy UNION
-	select distinct 'dx_autoimmune'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_autoimmune UNION
-	select distinct 'dx_cancer'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_cancer UNION
-	select distinct 'dx_diabetes'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_diabetes UNION
-	select distinct 'dx_heart'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_heart UNION
-	select distinct 'dx_htn'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_htn UNION
-	select distinct 'dx_immunocompromised'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_immunocompromised UNION
-	select distinct 'dx_infection'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_infection UNION
-	select distinct 'dx_kidney'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_kidney UNION
-	select distinct 'dx_transplant'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_transplant UNION
-	select distinct 'lab_autoimmune'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_autoimmune UNION
-	select distinct 'lab_cbc'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_cbc UNION
-	select distinct 'lab_cmp'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_cmp UNION
-	select distinct 'lab_creatinine'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_creatinine UNION
-	select distinct 'lab_diabetes'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_diabetes UNION
-	select distinct 'lab_gfr'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_gfr UNION
-	select distinct 'lab_lft'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_lft UNION
-	select distinct 'proc_dialysis'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_proc_dialysis UNION
-	select distinct 'proc_nephrectomy'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_proc_nephrectomy UNION
-	select distinct 'proc_surgery'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_proc_surgery UNION
-	select distinct 'proc_transplant'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_proc_transplant UNION
-	select distinct 'rx_antibiotics'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_antibiotics UNION
-	select distinct 'rx_cancer'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_cancer UNION
-	select distinct 'rx_diabetes'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_diabetes UNION
-	select distinct 'rx_diuretics'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_diuretics UNION
-	select distinct 'rx_htn'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_htn UNION
-	select distinct 'rx_immunosuppressive'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_immunosuppressive UNION
-	select distinct 'lab_custom'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_custom UNION
+	select distinct 'doc_biopsy'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_doc_biopsy UNION ALL
+	select distinct 'dx_autoimmune'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_autoimmune UNION ALL
+	select distinct 'dx_cancer'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_cancer UNION ALL
+	select distinct 'dx_diabetes'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_diabetes UNION ALL
+	select distinct 'dx_heart'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_heart UNION ALL
+	select distinct 'dx_htn'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_htn UNION ALL
+	select distinct 'dx_immunocompromised'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_immunocompromised UNION ALL
+	select distinct 'dx_infection'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_infection UNION ALL
+	select distinct 'dx_kidney'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_kidney UNION ALL
+	select distinct 'dx_transplant'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_dx_transplant UNION ALL
+	select distinct 'lab_autoimmune'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_autoimmune UNION ALL
+	select distinct 'lab_cbc'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_cbc UNION ALL
+	select distinct 'lab_cmp'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_cmp UNION ALL
+	select distinct 'lab_creatinine'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_creatinine UNION ALL
+	select distinct 'lab_diabetes'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_diabetes UNION ALL
+	select distinct 'lab_gfr'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_gfr UNION ALL
+	select distinct 'lab_lft'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_lft UNION ALL
+	select distinct 'proc_dialysis'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_proc_dialysis UNION ALL
+	select distinct 'proc_nephrectomy'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_proc_nephrectomy UNION ALL
+	select distinct 'proc_transplant'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_proc_transplant UNION ALL
+	select distinct 'rx_antibiotics'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_antibiotics UNION ALL
+	select distinct 'rx_cancer'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_cancer UNION ALL
+	select distinct 'rx_diabetes'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_diabetes UNION ALL
+	select distinct 'rx_diuretics'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_diuretics UNION ALL
+	select distinct 'rx_htn'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_htn UNION ALL
+	select distinct 'rx_immunosuppressive'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_immunosuppressive UNION ALL
+	select distinct 'lab_custom'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_lab_custom UNION ALL
 	select distinct 'rx_custom'	 as variable, valueset, code, display, system, encounter_ref, subject_ref  from irae__cohort_rx_custom
 )
 select distinct
