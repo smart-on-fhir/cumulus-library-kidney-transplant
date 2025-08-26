4ecc1d1062f47f38895fbd4da40b5c2cbe17999e
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-08-26 09:11:01 -0700

    updated SQL styles per Matt suggestions. These are cosmetic changes and not functional changes.

--- cumulus_library_kidney_transplant/count/term_freq.py
+++ cumulus_library_kidney_transplant/count/term_freq.py
@@ -8,26 +8,27 @@ def by_subject(from_table:str, column:str, where=None, asc_desc:str = 'ASC'):
     return rank_value(from_table, column, where, 'subject_ref', asc_desc)
 
 def rank_value(from_table:str, column:str, where=None, group_ref:str ='subject_ref', asc_desc:str = 'ASC'):
-
     where_basic = f" {column} is NOT null and {column} != 'NotMentioned' and {column} != 'NA'"
     if  where:
         where = where_basic + where
     else:
         where = where_basic
 
-    sql = [f"WITH value_counts as (",
-           f"\tselect {group_ref}, {column}, count(*) as cnt",
-           f"\tfrom {from_table}",
-           f"\twhere {where}",
-           f"\tgroup by {group_ref}, {column}", "),",
-           f"ranked_tf as (",
-           f"\tselect {group_ref}, {column}, cnt,",
-           f"\trow_number() over ( partition by {group_ref} order by cnt DESC, {column} {asc_desc}) as rn",
-           "\tfrom value_counts )",
-           f"SELECT {group_ref}, {column} as {column}_best, cnt from ranked_tf",
-           f"where rn =1 order by {group_ref}"]
-    return ' \n'.join(sql)
-
+    return f"""WITH value_counts as (
+        select {group_ref}, {column}, count(*) as cnt
+        from {from_table}
+        where {where}
+        group by {group_ref}, {column}
+    ),
+    ranked_tf as (
+        select {group_ref}, {column}, cnt,
+        row_number() over ( 
+            partition by {group_ref} order by cnt DESC, {column} {asc_desc}
+        ) as rn
+        from value_counts 
+    )
+    SELECT {group_ref}, {column} as {column}_best, cnt from ranked_t,
+       where rn =1 order by {group_ref}"""
 
 if __name__ == "__main__":
     targets = [
--- tests/test_fhir2sql.py
+++ tests/test_fhir2sql.py
@@ -1,7 +1,4 @@
 import unittest
-import csv
-from typing import List
-from pathlib import Path
 from cumulus_library_kidney_transplant import guard, fhir2sql, filetool
 
 class TestFHIR2sql(unittest.TestCase):
@@ -36,14 +33,13 @@ class TestFHIR2sql(unittest.TestCase):
                        'lab','imaging','count_sum']
 
         file_csv = filetool.path_spreadsheet('casedef_custom.csv')
-        actual = csv_headers(file_csv)
+        actual = fhir2sql.csv_headers(file_csv)
         self.assertEqual(header_list, actual)
 
-    def ignore_test_criteria2view(self):
+    def ignore_test_csv2view(self):
         file_csv = filetool.path_spreadsheet('casedef_custom.csv')
         view_name = fhir2sql.name_prefix('casedef_custom_csv')
         view_file = filetool.path_athena(f'{view_name}.sql')
 
-        _sql = csv2sql(file_csv, view_name)
-        filetool.write_text(_sql, view_file)
+        fhir2sql.csv2view(file_csv, view_name)
 
