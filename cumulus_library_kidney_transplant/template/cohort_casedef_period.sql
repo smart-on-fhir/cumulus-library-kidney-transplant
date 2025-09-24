6c88a88efea2945952dcdbac5efb0e93e30e0143
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-09-24 12:59:49 -0700

    Removed CamelCase per request of @dogversioning and used the snake_case instead. Attempted to use most stylistic conventions
    https://docs.telemetry.mozilla.org/concepts/sql_style#variable-names

--- cumulus_library_kidney_transplant/template/cohort_casedef_period.sql
+++ cumulus_library_kidney_transplant/template/cohort_casedef_period.sql
@@ -14,17 +14,19 @@
 
 create table $prefix__cohort_casedef_$period as
 WITH
-IndexDate as
-(
+first_visit as (
     select      min(enc_period_start_day) as index_date,
                 subject_ref
     from        $prefix__cohort_casedef_include
     group by    subject_ref
 )
-select  IndexDate.index_date,
+select
+        first_visit.index_date,
         SP.*
-from    $prefix__cohort_study_population as SP,
-        IndexDate
-where   SP.subject_ref = IndexDate.subject_ref
-and     SP.enc_period_start_day $equality IndexDate.index_date
+from
+        $prefix__cohort_study_population as SP,
+        first_visit
+where
+        SP.subject_ref = first_visit.subject_ref
+and     SP.enc_period_start_day $equality first_visit.index_date
 ;
