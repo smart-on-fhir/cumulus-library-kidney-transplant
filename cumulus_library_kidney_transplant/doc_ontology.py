2776ab182ae6293ec279ab3ce5b4d31eb8dbc0c4
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-10-13 15:02:48 -0700

    process Athena from TEMPLATE dir

--- cumulus_library_kidney_transplant/doc_ontology.py
+++ cumulus_library_kidney_transplant/doc_ontology.py
@@ -1,6 +1,20 @@
 from typing import List
 from pathlib import Path
-from cumulus_library_kidney_transplant import filetool
+from cumulus_library_kidney_transplant import filetool, fhir2sql
 
 def make() -> List[Path]:
-    pass
+    targets = [
+        'doc_ontology',
+        'doc_ontology_kind',
+        'doc_ontology_role',
+        'doc_ontology_service',
+        'doc_ontology_domain',
+        'doc_ontology_include',
+    ]
+    for target in targets:
+        sql = filetool.load_template(f"{target}.sql")
+        view_name = fhir2sql.name_prefix(target)
+        filetool.save_athena_view(view_name, sql)
+
+if __name__ == "__main__":
+    make()
