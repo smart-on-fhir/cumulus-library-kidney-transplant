d92842234cecdea1851fd18484fc670967ace30e
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-10-15 12:17:46 -0700

    lab_hla.csv : initial import.

--- cumulus_library_kidney_transplant/loinc_names.py
+++ cumulus_library_kidney_transplant/loinc_names.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 import pandas as pd
 from cumulus_library_kidney_transplant import filetool, fhir2sql
 
@@ -28,31 +29,28 @@ def loinc2valueset() -> None:
 def hydrate(valueset_csv:Path, view_name:str) -> None:
     loinc_df = pd.read_csv(LOINC_VALUESET_CSV)
     valueset_df = pd.read_csv(valueset_csv)
-
-    valueset_keys = set(valueset_df['code'].dropna().unique())
-    loinc_keys = set(loinc_df['code'].dropna().unique())
+    valueset_col = set(list(valueset_df.columns)).intersection({'code', 'LOINC_NUM'}).pop()
+    valueset_codes = set(valueset_df[valueset_col].dropna().unique())
+    loinc_codes = set(loinc_df['code'].dropna().unique())
 
     print(valueset_csv)
-    print(len(valueset_keys), ' #valueset')
-    print(len(loinc_keys), ' #loinc (official)')
-    print(len(valueset_keys & loinc_keys), ' #intersection')
-    print(len(loinc_keys - valueset_keys), ' #difference loinc')
-    print(len(valueset_keys - loinc_keys), ' #difference valueset')
+    print(len(valueset_codes), ' #valueset')
+    print(len(loinc_codes), ' #loinc (official)')
+    print(len(valueset_codes & loinc_codes), ' #intersection')
+    print(len(loinc_codes - valueset_codes), ' #difference loinc')
+    print(len(valueset_codes - loinc_codes), ' #difference valueset')
 
-    if valueset_keys - loinc_keys:
-        print('missing ', valueset_keys - loinc_keys)
+    if valueset_codes - loinc_codes:
+        raise Exception('missing ', valueset_codes - loinc_codes)
 
-    # if not valueset_csv.exists():
-    #     print(valueset_csv, 'does not exist')
-
+    backup_csv = str(valueset_csv) + '.backup.csv'
     temp_csv = str(valueset_csv) + '.hydrate.csv'
 
-    filtered = loinc_df[loinc_df['code'].isin(valueset_keys)].copy()
+    shutil.copy(valueset_csv, backup_csv)
+
+    filtered = loinc_df[loinc_df['code'].isin(valueset_codes)].copy()
     filtered.to_csv(filetool.path_spreadsheet(temp_csv), index=False)
     fhir2sql.csv2view(Path(temp_csv), view_name)
 
 
-if __name__ == "__main__":
-    valueset_csv = filetool.path_spreadsheet('lab_hla.csv')
-    hydrate(filetool.path_spreadsheet(valueset_csv), 'irae__lab_hla')
 
