080a5a6f02c508b4dab402616128cc3ec1a78d91
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-10-17 11:41:14 -0700

    removed trailing "s"

--- tests/variable/test_spreadsheet.py
+++ tests/variable/test_spreadsheet.py
@@ -5,5 +5,3 @@ class TestSpreadsheet(unittest.TestCase):
     def test_list_variables(self):
         self.assertTrue('lab_hla' in custom_variables.list_variables_lab())
         self.assertTrue('rx_prednisone' in custom_variables.list_variables_rx())
-
-s
