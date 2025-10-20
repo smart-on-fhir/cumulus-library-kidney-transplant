b39f85f3beea7dd8ceea639029e8b4cef4e6421f
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-10-20 12:36:34 -0700

    tests deleting deprecated test files

--- tests/count/test_dashboard.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from cumulus_library_kidney_transplant.count import deprecated_dashboard
-
-class TestDashboard(unittest.TestCase):
-
-    def test_graph_list(self):
-        for graph in deprecated_dashboard.list_graph_defaults():
-            print(graph.as_json())
-
