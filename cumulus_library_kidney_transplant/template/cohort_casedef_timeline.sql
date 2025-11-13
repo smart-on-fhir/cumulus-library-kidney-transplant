804367cc3a8aefdab2227d3770817ad391001c68
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-11-13 15:15:37 -0800

    improving the timeline for the SOE sequence of events related to the case definition.

--- cumulus_library_kidney_transplant/template/cohort_casedef_timeline.sql
+++ cumulus_library_kidney_transplant/template/cohort_casedef_timeline.sql
@@ -1,9 +1,18 @@
 -- Timeline with "sequence of events" (soe) of patients matching case definition
 --
 create or replace view $prefix__cohort_casedef_timeline as
-select 'pre'   as casedef_period, * from $prefix__cohort_casedef_pre
+select 'pre'   as casedef_period, studyvars.*
+from    $prefix__cohort_casedef_pre as casedef,
+        $prefix__cohort_study_variables_wide as studyvars
+where   casedef.encounter_ref = studyvars.encounter_ref
 UNION ALL
-select 'index' as casedef_period, * from $prefix__cohort_casedef_index
+select 'index' as casedef_period, studyvars.*
+from    $prefix__cohort_casedef_index as casedef,
+        $prefix__cohort_study_variables_wide as studyvars
+where   casedef.encounter_ref = studyvars.encounter_ref
 UNION ALL
-select 'post'  as casedef_period, * from $prefix__cohort_casedef_post
+select 'post'  as casedef_period, studyvars.*
+from    $prefix__cohort_casedef_post as casedef,
+        $prefix__cohort_study_variables_wide as studyvars
+where   casedef.encounter_ref = studyvars.encounter_ref
 ;
