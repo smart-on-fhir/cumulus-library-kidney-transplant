cc33ece41c5930fceb8e63184340c7c1a0fa467d
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-11-21 19:16:56 -0800

    initial import "index_post"

---
+++ cumulus_library_kidney_transplant/athena/irae__sample_casedef_index_post.sql
@@ -0,0 +1,58 @@
+CREATE table irae__sample_casedef_index_post as
+WITH
+encounter_doc as (
+    SELECT  distinct
+            etl.group_name,
+            casedef.subject_ref,
+            casedef.encounter_ref,
+            case
+            when (doc.doc_author_day    is NOT null)    then doc.doc_author_day
+            when (doc.doc_date          is NOT null)    then doc.doc_date
+            else doc.enc_period_start_day               end as sort_by_date,
+            doc.documentreference_ref,
+            doc.doc_author_day,
+            doc.doc_date,
+            doc.enc_period_start_day,
+            doc.enc_period_ordinal,
+            doc.doc_type_code,
+            doc.doc_type_display,
+            doc.doc_type_system
+    FROM
+            etl__completion_encounters              as etl,
+            irae__cohort_casedef_index_post         as casedef,
+            irae__cohort_study_population_doc    as doc
+    WHERE
+            casedef.encounter_ref   = doc.encounter_ref
+    AND     casedef.encounter_ref   = concat('Encounter/', etl.encounter_id)
+    ORDER BY
+            casedef.subject_ref
+),
+encounter_doc_uniq as
+(
+    SELECT  DISTINCT
+            subject_ref,
+            documentreference_ref,
+            sort_by_date
+    FROM    index_post
+)
+ordered as (
+    SELECT  distinct
+            subject_ref,
+            documentreference_ref,
+            sort_by_date,
+            ROW_NUMBER() OVER (
+                PARTITION   BY  subject_ref
+                ORDER       BY  sort_by_date,
+                                documentreference_ref
+            )   AS doc_ordinal
+    FROM    encounter_doc_uniq
+)
+SELECT  distinct
+        encounter_doc.*,
+        ordered.doc_ordinal
+FROM
+        ordered,
+        encounter_doc
+WHERE
+        ordered.documentreference_ref = encounter_doc.documentreference_ref
+;
