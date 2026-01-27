5f4cb3c8045fdae32c476584738e702853a525f4
comorbidity <andrew.mcmurry@tch.harvard.edu>
2026-01-27 09:41:35 -0800

    backport cohort_casedef.sql with the corresponding aspects for diagnosis, medications, procedures, lab

---
+++ cumulus_library_kidney_transplant/template/cohort_casedef_dx.sql
@@ -0,0 +1,22 @@
+create  table $prefix__cohort_casedef_dx as
+WITH
+casedef as
+(
+    select  days_since,
+            ordinal_since,
+            dx_system,
+            coalesce(dx_code, 'NO_CODE')        as dx_code,
+            coalesce(dx_display, 'NO_DISPLAY')  as dx_display,
+            subject_ref,
+            encounter_ref
+    from    $prefix__cohort_casedef
+)
+select  distinct
+        casedef.days_since,
+        casedef.ordinal_since,
+        dx.*
+from    casedef,
+        $prefix__cohort_study_population_dx as dx
+where   casedef.subject_ref = dx.subject_ref
+and     (dx.dx_code, dx.dx_system) not in
+        (select distinct code, system from $prefix__valueset_casedef)
