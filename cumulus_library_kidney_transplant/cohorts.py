0e53d55f0ceaa9863d5a5702015b960e51ebed8f
comorbidity <andrew.mcmurry@tch.harvard.edu>
2026-01-27 11:40:42 -0800

    added UNION and WIDE variable cohort representations.

--- cumulus_library_kidney_transplant/cohorts.py
+++ cumulus_library_kidney_transplant/cohorts.py
@@ -94,7 +94,72 @@ def cohort_doc(variable: str) -> Path:
 ###############################################################################
 # Select Variables UNION and WIDE representation
 ###############################################################################
-def make_study_variables_union() -> Path:
+def select_union(variable_list: list[str]) -> str:
+    """
+    :param variable_list: variable names (typically list of valuesets)
+    :return: str SQL select UNION ALL for the provided variable list
+    """
+    sql = list()
+    for variable in variable_list:
+        variable = fhir2sql.name_simple(variable)
+        select = f"\tselect distinct '{variable}'\t as variable, code, display, system, encounter_ref "
+        from_table = f" from {fhir2sql.PREFIX}__cohort_{variable}"
+        sql.append(select + from_table)
+    return ' UNION ALL\n'.join(sql)
+
+def select_lookup_variable_as_column(variable_list: list[str]) -> str:
+    """
+    :param variable_list: variable names (typically list of valuesets)
+    :return: str SQL select variable table as column name
+    """
+    sql = list()
+    for variable in variable_list:
+        variable = fhir2sql.name_simple(variable)
+        sql.append(f"\tIF(lookup.variable='{variable}', True) AS {variable}")
+    return ',\n'.join(sql)
+
+def select_lookup_wide(variable_list: list[str]) -> str:
+    """
+    :param variable_list: variable names (typically list of valuesets)
+    :return: str SQL select variable for any arbitrary match (typically on the FHIR encounter)
+    """
+    sql = list()
+    for variable in variable_list:
+        variable = fhir2sql.name_simple(variable)
+        sql.append(f"\tarbitrary({variable})    FILTER (where {variable} ) as {variable}")
+    return ',\n'.join(sql)
+
+def make_union() -> Path:
+    """
+    All study variable cohorts in one table.
+    "see `template/cohort_study_variables.sql`"
+    :return: Path to SQL file for each study variable 1+ `valueset`
+    """
+    template_sql = filetool.load_template(f"cohort_variable_union.sql")
+    template_sql = template_sql.replace('$variable_list', select_union(list_variables()))
+    cohort_name = fhir2sql.name_cohort('variable_union')
+    target_file = filetool.path_athena(f"{cohort_name}.sql")
+
+    return filetool.save_athena(target_file, template_sql)
+
+def make_wide() -> Path:
+    """
+    All study variable cohorts in one table in WIDE format.
+    each column is a study variable.
+
+    see `template/cohort_study_variables_wide.sql`
+    :return: Path to SQL file `athena/irae__cohort_study_variables_wide.sql`
+    """
+    variable_list = list_variables()
+    template_sql = filetool.load_template(f"cohort_variable_wide.sql")
+    template_sql = template_sql.replace('$variable_list_lookup', select_lookup_variable_as_column(variable_list))
+    template_sql = template_sql.replace('$variable_list_wide', select_lookup_wide(variable_list))
+    cohort_name = fhir2sql.name_cohort('variable_wide')
+    target_file = filetool.path_athena(f"{cohort_name}.sql")
+
+    return filetool.save_athena(target_file, template_sql)
+
+def make_study_variables_union_deprecated() -> Path:
     """
     All study variable cohorts in one table.
 
@@ -104,10 +169,10 @@ def make_study_variables_union() -> Path:
     select = fhir2sql.select_union_study_variables(list_variables())
     file = fhir2sql.name_study_variables() + '.sql'
     text = filetool.load_template(file)
-    text = filetool.replace_text(text, {'variable':select})
+    text = filetool.replace_text(text, {'variables':select})
     return filetool.save_athena(file, text)
 
-def make_study_variables_wide() -> Path:
+def make_study_variables_wide_deprecated() -> Path:
     """
     All study variable cohorts in one table in WIDE format.
     each column is a study variable.
@@ -123,7 +188,7 @@ def make_study_variables_wide() -> Path:
     text = text.replace('$variable_list_wide', wide)
     return filetool.save_athena(file, text)
 
-def make_study_variables_timeline() -> Path:
+def make_study_variables_timeline_deprecated() -> Path:
     """
     All study variable cohorts in one table in WIDE format.
     each column is a study variable.
@@ -189,8 +254,9 @@ def make() -> List[Path]:
     :return: List of SQL files for each study variable COHORT.
     """
     variables_each = make_each_study_variable()
-    variables_union = [make_study_variables_union(),
-                       make_study_variables_wide(),
-                       make_study_variables_timeline()]
+    variables_all = [make_union(), make_wide()]
+    #variables_union = [make_study_variables_union_deprecated(),
+    #                   make_study_variables_wide_deprecated(),
+    #                   make_study_variables_timeline_deprecated()]
 
-    return variables_each + variables_union
+    return variables_each + variables_all
