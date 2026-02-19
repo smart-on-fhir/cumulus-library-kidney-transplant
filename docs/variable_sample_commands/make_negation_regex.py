8f2c7b523b5d594d56b7b70c347b1c53c09fcb57
Dylan Phelan <Dtphelan1@gmail.com>
2026-02-19 17:33:47 -0500

    WIP on pipeline going from keywords TSV to computable sample commands - negative regex generation no longer uses list divorced from TSV

--- docs/variable_sample_commands/make_negation_regex.py
+++ docs/variable_sample_commands/make_negation_regex.py
@@ -1,4 +1,5 @@
 import re
+import json
 from pathlib import Path
 
 def generate_exclusion_regex(patterns):
@@ -14,235 +15,25 @@ def generate_exclusion_regex(patterns):
     # using .*$ at the end causes a catastrophic backtracking problem
     return re.compile(rf"^(?!.*(?:{combined_patterns}))")
 
-def get_exclusion_patterns():
-    keywords = [
-        ###################
-        # Donor Relationship 
-        "KDIGO",
-        "Related Donor",
-        "Familial donor",
-        "Family donor",
-        "Sibling donor",
-        "Parent donor",
-        "Mother donor",
-        "Father donor",
-        "Child donor",
-        "Brother donor",
-        "Sister donor",
-        "Unrelated donor",
-        "directed donor",
-        "altruistic donor",
-        "good samaritan donor",
-        "anonymous donor",
-        "volunteer donor",
-        "NDD",
-        "non directed donor",
-        "non-directed donor",
-        "nondirected donor",
-        "LRD",
-        "LRRT",
-        "living related donor",
-        "living relative",
-        "LURD",
-        "living unrelated donor",
-        ###################
-        # Donor Sero CMV 
-        "Recipient",
-        "Transplant",
-        "Donor",
-        "Donation",
-        "Organ",
-        "donor serostatus",
-        "D+",
-        "D +",
-        "D(+)",
-        "D (+)",
-        "D-pos",
-        "Donor pos",
-        "Donor+",
-        "Donor +",
-        "Donor(+)",
-        "Donor (+)",
-        "Donor reactive",
-        "Donor-positive",
-        "D-",
-        "D -",
-        "D(-)",
-        "D (-)",
-        "D-neg",
-        "Donor neg",
-        "Donor-",
-        "Donor -",
-        "Donor(-)",
-        "Donor (-)",
-        "Donor nonreactive",
-        "Donor-negative",
-        "serostatus",
-        "seropositive",
-        "seropositivity",
-        "IgG positive",
-        "reactive",
-        "antibody detected",
-        "seronegative",
-        "seronegativity",
-        "IgG negative",
-        "non-reactive",
-        "no antibodies detected",
-        "serology",
-        "viral status",
-        "titer",
-        "screen",
-        "NAT",
-        "nucleic acid test",
-        "PCR",
-        "viral load",
-        "Ig markers",
-        "IgG",
-        "IgM",
-        "Total Ab",
-        "Antibody",
-        "Ab",
-        "Cytomegalovirus",
-        "CMV",
-        "CMV+",
-        "CMV-",
-        ###################
-        # Unique Donor Serostatus EBV
-        "Epstein-Barr",
-        "Epstein",
-        "EBV",
-        "EBV+",
-        ###################
-        # Unique Donor Status
-        "Living Donor",
-        "Living Kidney",
-        "Living Renal",
-        "Live Donor",
-        "Live Renal",
-        "Live Kidney",
-        "LDRT",
-        "Deceased Donor",
-        "Deceased Kidney",
-        "Deceased Renal",
-        "Deceased Organ",
-        "Heart beating",
-        "Beating heart",
-        "Brain dead",
-        "Brain-dead",
-        "Cadaveric",
-        "cadaver",
-        "DD donor",
-        "DD kidney",
-        "DD renal",
-        "Renal DD",
-        "Kidney DD",
-        ###################
-        # Unique HLA Mismatch
-        "HLA",
-        "human leukocyte antigen",
-        "antigen matched",
-        "antigen matches",
-        "antigen match",
-        "mismatch",
-        "mismatches",
-        "mismatched",
-        "well matched",
-        "good match",
-        "excellent match",
-        "excellently matched",
-        "favorable match",
-        "favorably matched",
-        "close match",
-        "closely matched",
-        "fully matched",
-        "fully matches",
-        "full match",
-        "complete match",
-        "completely matched",
-        "perfect match",
-        "perfectly matched",
-        "identical match",
-        "identically matched",
-        "moderately matched",
-        "moderate match",
-        "partial match",
-        "partially match",
-        "intermediate match",
-        "acceptable match",
-        "acceptably matched",
-        "suitable match",
-        "compatible match",
-        "poorly matched",
-        "poor match",
-        "weak match",
-        "weakly matched",
-        "high mismatch",
-        "highly mismatched",
-        "unfavorable match",
-        "unfavorably matched",
-        "DSA",
-        "DSAs",
-        "Donor specific",
-        ###################
-        # Unique HLA Mismatch
-        "HLA Quality",
-        "Highly Sensitized",
-        "Alloimmunized",
-        "Desensitization",
-        "Desensitized",
-        ###################
-        # Unique Recipient Serostatus
-        "recipient serostatus",
-        "R+",
-        "R +",
-        "R(+)",
-        "R (+)",
-        "R-pos",
-        "Recip pos",
-        "Recip+",
-        "Recip +",
-        "Recip(+)",
-        "Recip (+)",
-        "Recipient reactive",
-        "Recip-positive",
-        "R-",
-        "R -",
-        "R(-)",
-        "R (-)",
-        "R-neg",
-        "Recip neg",
-        "Recip-",
-        "Recip -",
-        "Recip(-)",
-        "Recip (-)",
-        "Recipient nonreactive",
-        "Recip-negative",
-        ###################
-        # Unique Transplant Date
-        "Transplant Date" ,
-        "transplanted on",
-        "POD",
-        "transplant on" ,
-        re.compile(r"now day\s*\+?\d+"),
-        re.compile(r"transplant\s*\(?\d+\s*/\s*\d+\)?"),
-        ###################
-        # Avoid Noise from 
-        # Unprocessed Note Text 
-        "OperationOutcome",
-    ]
-    
+def get_exclusion_patterns(keyword_set: set):
     # Escape patterns 
     patterns = []
-    for k in keywords:
-        if isinstance(k, re.Pattern):
-            patterns.append(k.pattern)
+    for k in keyword_set:
+        if k.startswith('REGEX:'):
+            patterns.append(re.compile(k.split('REGEX:')[1].strip()).pattern)
         else:
             patterns.append(re.escape(k))
     return patterns
 
 def make_negation_regex():
+    keyword_lookup_path = Path(__file__).parent / 'keyword_lookup.json'
+    keyword_lookup = json.load(keyword_lookup_path.open())
+    keyword_set = set()
+    for keyword_group in keyword_lookup.values(): 
+        keyword_set.update(set(keyword_group['keywords']))
+    
     # --- Example Usage ---
-    keywords_and_regexes = get_exclusion_patterns()
+    keywords_and_regexes = get_exclusion_patterns(keyword_set)
     master_regex = generate_exclusion_regex(keywords_and_regexes)
     print(f"Generated Regex: {master_regex}\n")
     
