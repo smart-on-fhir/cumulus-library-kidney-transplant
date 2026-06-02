55761336fac4b514142eee5ed6cac8fb9c7132cd
comorbidity <andrew.mcmurry@tch.harvard.edu>
2026-06-02 13:07:47 -0700

    irae__cohort_casedef_exclude.sql was updated to remove dx_transplant and proc_transplant I think this was never correct.

--- cumulus_library_kidney_transplant/athena/irae__cohort_casedef_exclude.sql
+++ cumulus_library_kidney_transplant/athena/irae__cohort_casedef_exclude.sql
@@ -1,31 +1,32 @@
 CREATE TABLE irae__cohort_casedef_exclude as WITH
-dx_transplant AS
-(
-    SELECT  DISTINCT
-            'dx_transplant' as variable, system, code, display,
-            subject_ref
-    FROM    irae__cohort_dx_transplant  AS cohort
-    WHERE   CODE NOT IN ('Z94.9', 'V42.9') -- https://github.com/smart-on-fhir/cumulus-library-kidney-transplant/issues/39
-    AND     NOT EXISTS (
-                SELECT  1
-                FROM    irae__casedef   AS casedef
-                WHERE   casedef.code    = cohort.code
-                AND     casedef.system  = exclude.system
-            )
-),
-proc_transplant AS
-(
-    SELECT  DISTINCT
-            'proc_transplant' as variable, system, code, display,
-            subject_ref
-    FROM    irae__cohort_proc_transplant AS cohort
-    WHERE   NOT EXISTS (
-                SELECT  1
-                FROM    irae__casedef   AS casedef
-                WHERE   casedef.code    = cohort.code
-                AND     casedef.system  = cohort.system
-            )
-),
+-- https://github.com/smart-on-fhir/cumulus-library-kidney-transplant/issues/39
+--    dx_transplant AS
+--    (
+--        SELECT  DISTINCT
+--                'dx_transplant' as variable, system, code, display,
+--                subject_ref
+--        FROM    irae__cohort_dx_transplant  AS cohort
+--        WHERE   CODE NOT IN ('Z94.9', 'V42.9')
+--        AND     NOT EXISTS (
+--                    SELECT  1
+--                    FROM    irae__casedef   AS casedef
+--                    WHERE   casedef.code    = cohort.code
+--                    AND     casedef.system  = exclude.system
+--                )
+--    ),
+--    proc_transplant AS
+--    (
+--        SELECT  DISTINCT
+--                'proc_transplant' as variable, system, code, display,
+--                subject_ref
+--        FROM    irae__cohort_proc_transplant AS cohort
+--        WHERE   NOT EXISTS (
+--                    SELECT  1
+--                    FROM    irae__casedef   AS casedef
+--                    WHERE   casedef.code    = cohort.code
+--                    AND     casedef.system  = cohort.system
+--                )
+--    ),
 first_visit AS
 (
     SELECT  MIN(enc_period_start_day) AS index_date,
@@ -47,21 +48,22 @@ first_visit_complication AS
     SELECT  DISTINCT
             valueset, system, code, display,
             complication.subject_ref
-    FROM    first_visit, complication
-    WHERE   first_visit.subject_ref = complication.subject_ref
+    FROM    first_visit
+    JOIN    complication
+    ON      first_visit.subject_ref = complication.subject_ref
     AND     first_visit.index_date > complication.index_date
 ),
 exclusion_list AS
 (
-    select * from dx_transplant
-    UNION ALL
-    select * from proc_transplant
-    UNION ALL
+--    select * from dx_transplant
+--    UNION ALL
+--    select * from proc_transplant
+--    UNION ALL
     select * from first_visit_complication
 )
 SELECT  DISTINCT
         exclusion_list.*
-FROM    exclusion_list,
-        irae__cohort_casedef_candidate AS casedef
-WHERE   exclusion_list.subject_ref = casedef.subject_ref
+FROM    exclusion_list
+JOIN    irae__cohort_casedef_candidate AS casedef
+ON      exclusion_list.subject_ref = casedef.subject_ref
 ;
